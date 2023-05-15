d29fdc32fc83888ca22f9778d642221f41da747d
Georgeches <georgechesire83@gmail.com>
2023-05-15 12:36:05 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,20 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Jimmy")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(a,b)
+    return a + b
+end
+
+def halve(i)
+    i.is_a?(Integer) ? (return i/2) : (return nil)
+end
